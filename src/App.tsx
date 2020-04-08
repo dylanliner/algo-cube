3c6b5acd94491440136479f04f6020b0734e23f6
cap3k <37514740+cap3k@users.noreply.github.com>
2020-04-08 22:19:11 +0200

    feat: Selection Modes + select start and end node

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,73 @@ import { Canvas, useFrame } from "react-three-fiber";
 import Box from "./components/Box";
 import { BoxObject } from "./components/Box";
 import update from "immutability-helper";
+import Button from "@material-ui/core/Button";
+
+enum SelectionMode {
+  StartNode,
+  EndNode,
+  BlockedNodes,
+  Final,
+}
 
 const App: React.FC = () => {
   const size = 10;
 
-  console.log("I am the grid and am rerendering");
-  const [boxes, setBoxes] = useState([
-    {
-      index: 0,
-      position: [0, 0, 0] as [number, number, number],
-      isBlocked: false,
-      updateBlocked: (index: number, isBlocked: boolean) => {
-        console.log("stuff");
+  const [reload, setReload] = useState(true);
+  const [grid, setGrid] = useState({
+    selectionMode: SelectionMode.StartNode,
+    boxes: [
+      {
+        index: 0,
+        position: [0, 0, 0] as [number, number, number],
+        isBlocked: false,
+        isStartNode: false,
+        isEndNode: false,
+        updateBox: (index: number) => {
+          console.log("stuff");
+        },
       },
-    },
-  ]);
+    ],
+  });
+  console.log("I am the grid and am rerendering");
 
-  const cameraCenter = size / 2;
+  const cameraCenter = size / 2 - 1;
 
   useEffect(() => {
     console.log("I am in useEffect");
-    const updateBlocked = (index: number, isBlocked: boolean): void => {
-      console.log(index + " is blocked " + isBlocked);
-
-      setBoxes((boxes) =>
-        update(boxes, { [index]: { isBlocked: { $set: isBlocked } } })
-      );
+    const updateBox = (index: number): void => {
+      setGrid((grid) => {
+        const prevBox = grid.boxes[index];
+        const newBox = Object.assign({}, prevBox);
+        let newSelectionMode = grid.selectionMode;
+        console.log(grid.selectionMode);
+        switch (grid.selectionMode) {
+          case SelectionMode.StartNode:
+            if (!newBox.isBlocked) {
+              newBox.isStartNode = true;
+              newSelectionMode = SelectionMode.EndNode;
+            }
+            break;
+          case SelectionMode.EndNode:
+            if (!newBox.isStartNode && !newBox.isBlocked) {
+              newBox.isEndNode = true;
+              newSelectionMode = SelectionMode.Final;
+            }
+            break;
+          case SelectionMode.Final:
+            console.log("reloading");
+            setReload(!reload);
+            break;
+          case SelectionMode.BlockedNodes:
+            newBox.isBlocked = !prevBox.isBlocked;
+            break;
+        }
+        console.log("new props", index, newBox);
+        return {
+          selectionMode: newSelectionMode,
+          boxes: update(grid.boxes, { [index]: { $set: newBox } }),
+        };
+      });
     };
     function createGrid() {
       console.log("I am in create Grid");
@@ -40,18 +81,23 @@ const App: React.FC = () => {
             index: 0,
             position: [i, y, 0] as [number, number, number],
             isBlocked: false,
-            updateBlocked: updateBlocked,
+            isStartNode: false,
+            isEndNode: false,
+            updateBox: updateBox,
           });
         }
       }
-      setBoxes(boxArray);
+      setGrid({
+        selectionMode: SelectionMode.StartNode,
+        boxes: boxArray,
+      });
     }
     createGrid();
-  }, []);
+  }, [reload]);
 
   const style = {
-    width: "100vw",
-    height: "100vh",
+    width: "90vw",
+    height: "90vh",
   };
 
   function CustomCamera() {
@@ -64,21 +110,51 @@ const App: React.FC = () => {
     return null;
   }
 
+  const handleChange = (): void => {
+    setGrid((grid) => {
+      let newGridSelectionMode;
+      if (grid.selectionMode === SelectionMode.BlockedNodes) {
+        if (grid.boxes.some((box) => box.isEndNode)) {
+          newGridSelectionMode = SelectionMode.Final;
+        } else if (grid.boxes.some((box) => box.isStartNode)) {
+          newGridSelectionMode = SelectionMode.EndNode;
+        } else {
+          newGridSelectionMode = SelectionMode.StartNode;
+        }
+      } else {
+        newGridSelectionMode = SelectionMode.BlockedNodes;
+      }
+      return {
+        boxes: grid.boxes,
+        selectionMode: newGridSelectionMode,
+      };
+    });
+  };
+
   return (
-    <Canvas style={style}>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      {boxes.map((boxProps: BoxObject, index: number) => (
-        <Box
-          key={index.toString()}
-          index={index}
-          position={boxProps.position}
-          updateBlocked={boxProps.updateBlocked}
-          isBlocked={boxProps.isBlocked}
-        />
-      ))}
-      <CustomCamera />
-    </Canvas>
+    <>
+      <Button onClick={handleChange} variant="contained">
+        {grid.selectionMode === SelectionMode.BlockedNodes
+          ? "Click here when you are done"
+          : "Click here to select block nodes"}
+      </Button>
+      <Canvas style={style}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        {grid.boxes.map((boxProps: BoxObject, index: number) => (
+          <Box
+            key={index.toString()}
+            index={index}
+            position={boxProps.position}
+            updateBox={boxProps.updateBox}
+            isBlocked={boxProps.isBlocked}
+            isStartNode={boxProps.isStartNode}
+            isEndNode={boxProps.isEndNode}
+          />
+        ))}
+        <CustomCamera />
+      </Canvas>
+    </>
   );
 };
 
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useFrame } from "react-three-fiber";
 import * as THREE from "three";
 
@@ -6,7 +6,9 @@ export interface BoxObject {
   index: number;
   position: [number, number, number];
   isBlocked: boolean;
-  updateBlocked: (index: number, isBlocked: boolean) => void;
+  isStartNode: boolean;
+  isEndNode: boolean;
+  updateBox: (index: number) => void;
 }
 
 const Box: React.FC<BoxObject> = React.memo((props: BoxObject) => {
@@ -16,10 +18,28 @@ const Box: React.FC<BoxObject> = React.memo((props: BoxObject) => {
 
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
-
+  const [color, setColor] = useState("orange");
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
+  useEffect(() => {
+    const getColor = (): void => {
+      let color = "";
+      if (props.isBlocked) {
+        color = "hotpink";
+      } else if (props.isStartNode) {
+        color = "blue";
+      } else if (props.isEndNode) {
+        color = "green";
+      } else if (hovered) {
+        color = "red";
+      } else {
+        color = "orange";
+      }
+      setColor(color);
+    };
+    getColor();
+  }, [hovered, props.isBlocked, props.isEndNode, props.isStartNode]);
   return (
     <mesh
       position={props.position}
@@ -27,16 +47,13 @@ const Box: React.FC<BoxObject> = React.memo((props: BoxObject) => {
       scale={[0.5, 0.5, 0.5]}
       onClick={(e) => {
         console.log("clicked on box");
-        props.updateBlocked(props.index, !props.isBlocked);
+        props.updateBox(props.index);
       }}
       onPointerOver={(e) => setHover(true)}
       onPointerOut={(e) => setHover(false)}
     >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered || props.isBlocked ? "hotpink" : "orange"}
-      />
+      <meshStandardMaterial attach="material" color={color} />
     </mesh>
   );
 });
